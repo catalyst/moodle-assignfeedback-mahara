2d2199eec42de2433cf953ecff525f85bc811ae9
Aaron Wells <aaronw@catalyst.net.nz>
2014-08-20 17:53:54 +1200

    Applying formatting

--- settings.php
+++ settings.php
@@ -1,9 +1,8 @@
 <?php
 
-
 $settings->add(new admin_setting_configcheckbox(
-  'assignfeedback_mahara/default',
-   new lang_string('default', 'assignfeedback_mahara'),
-   new lang_string('default_help', 'assignfeedback_mahara'),
-   1
+    'assignfeedback_mahara/default',
+    new lang_string('default', 'assignfeedback_mahara'),
+    new lang_string('default_help', 'assignfeedback_mahara'),
+    1
 ));
